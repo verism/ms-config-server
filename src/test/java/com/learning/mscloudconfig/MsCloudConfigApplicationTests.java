19bb401cf4a71531be3146663c8219515cddb3b5
Ravi Gupta <ravi.gupta@yahoo.in>
2021-05-27 18:21:30 +0000

    fix: update

---
+++ src/test/java/com/learning/mscloudconfig/MsCloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.mscloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCloudConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
